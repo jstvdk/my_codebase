ab12ecc43a02695e94a9e053c0e4afb9423755b4
Vadym Voitsekhovskyi <vadym.voitsekhovskyi@cta-consortium.org>
2024-11-06 17:34:46 +0100

    New PSF metric for errors

--- bash/find_word.sh
+++ bash/find_word.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DIRECTORY="/Users/vdk/Software/ctasoft/lst-sim-config"
+DIRECTORY="/Users/vdk/Software/ctasoft/aiv_integration/test-report"
 
 for FILE in "$DIRECTORY"/*
 do
    if [ -f "$FILE" ]; then
-      grep -sH "nightsky_background" "$FILE"
+      grep -sH ".*operation_call*." "$FILE"
    fi
 done
 
